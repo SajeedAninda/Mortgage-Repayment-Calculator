8746041ba0484280d93f45221b63c5dfd3c4344a
SajeedAninda <sajaninda@gmail.com>
2024-07-02 17:14:28 +0600

    Completed calculate section

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calcImage from "../assets/icon-calculator.svg"
 
 const Calculator = () => {
+    let [selectedOption, setSelectedOption] = useState('');
+
+    let handleOptionChange = (e) => {
+        setSelectedOption(e.target.value);
+    };
+
     return (
         <div className='bg-[#e3f3fd] h-screen flex items-center w-full mx-auto rounded-l-2xl'>
             <div className='w-[70%] mx-auto flex items-center bg-white rounded-l-2xl '>
                 <div className='calcDiv w-[50%] h-full bg-white rounded-l-2xl p-8'>
                     <div className='w-full flex justify-between items-center'>
                         <h2 className='text-[20px] text-[#122f37] font-bold'>Mortgage Calculator</h2>
-                        <p className='text-[#4E6E7E] text-[13px] underline hover:text-[#122f37]'>Clear All</p>
+                        <p className='text-[#4E6E7E] text-[13px] underline hover:text-[#122f37] cursor-pointer'>Clear All</p>
                     </div>
 
 
@@ -45,7 +52,7 @@ const Calculator = () => {
                                 </div>
 
 
-                             <div className='flex-1'>
+                                <div className='flex-1'>
                                     <label className='text-[16px] text-[#4E6E7E]' htmlFor="interestRate">Interest Rate</label>
                                     <div className='relative mt-2'>
                                         <label className='absolute inset-y-0 right-[1px] h-fit flex items-center px-3 py-3 bg-[#e3f3fd] text-[#4E6E7E] text-[16px] font-bold rounded-r-lg top-[1px]'>
@@ -61,7 +68,50 @@ const Calculator = () => {
                                 </div>
                             </div>
 
-                            
+                            <div className='mt-5'>
+                                <label className='text-[16px] text-[#4E6E7E]' htmlFor="mortgageType">Mortgage Type</label>
+                                <div className='mt-2'>
+                                    <label
+                                        className={`border flex justify-start items-center border-[#122f37] rounded-lg mb-2 px-3 py-3 cursor-pointer ${selectedOption === 'repayment' ? 'bg-[#DFE08A]' : ''}`}
+                                        htmlFor="repayment"
+                                    >
+                                        <input
+                                            type="radio"
+                                            id="repayment"
+                                            name="mortgageType"
+                                            value="repayment"
+                                            checked={selectedOption === 'repayment'}
+                                            onChange={handleOptionChange}
+                                            className="mr-2"
+                                        />
+                                        <span className='text-[#4E6E7E] font-bold text-[16px]'>
+                                            Repayment
+                                        </span>
+                                    </label>
+                                    <label
+                                        className={`border flex justify-start items-center border-[#122f37] rounded-lg px-3 py-3 cursor-pointer ${selectedOption === 'interestOnly' ? 'bg-[#DFE08A]' : ''}`}
+                                        htmlFor="interestOnly"
+                                    >
+                                        <input
+                                            type="radio"
+                                            id="interestOnly"
+                                            name="mortgageType"
+                                            value="interestOnly"
+                                            checked={selectedOption === 'interestOnly'}
+                                            onChange={handleOptionChange}
+                                            className="mr-2"
+                                        />
+                                        <span className='text-[#4E6E7E] font-bold text-[16px]'>
+                                            Interest Only
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <button className='mt-8 py-3 w-[70%] rounded-[45px] text-[16px] font-bold text-[#122f37] flex justify-center items-center gap-3 bg-[#D7DA2F] hover:bg-[#DFE08A]'>
+                                <img src={calcImage} alt="" />
+                                Calculate Repayments
+                            </button>
                         </form>
                     </div>
                 </div>
